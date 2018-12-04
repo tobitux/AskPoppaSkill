251979fa2cefdc9e38ccde1295ac6d70131569ac
Tobias Knapp <tobias.knapp@iti-software.de>
2018-12-04 22:16:44 +0100

    implement first skill

--- AskPoppaSkill/Function.cs
+++ AskPoppaSkill/Function.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+using Alexa.NET.Request;
+using Alexa.NET.Response;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -12,16 +9,20 @@ namespace AskPoppaSkill
 {
     public class Function
     {
-        
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+
+        public SkillResponse FunctionHandler(SkillRequest request)
         {
-            return input?.ToUpper();
+            return new SkillResponse()
+            {
+                Version = "0.1.0",
+                Response = new ResponseBody()
+                {
+                    OutputSpeech = new PlainTextOutputSpeech()
+                    {
+                        Text = "Nö"
+                    }
+                }
+            };
         }
     }
 }
